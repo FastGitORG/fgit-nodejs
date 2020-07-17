65a7f485e38ec9cf59810094ec5a843d0a882cf6
lihai2333 <lihai2333@hotmail.com>
2020-07-17 18:31:11 +0800

    git node版

---
+++ README.md
@@ -0,0 +1,3 @@
+# fgit nodejs版
+
+本工具由厉害制作，加速链接由[fastgit](https://hub.fastgit.org)提供。
---
+++ bin/fgit.js
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+
+var git = require("./../lib/git")
+var exec = require('child_process').exec;
+var argv = process.argv;
+var cloneRe = new RegExp("http[s]?://github.com/.*/.*|git@github.com:.*/.*", "i");
+
+if (!argv[2]) {
+		console.log("欢迎使用fgit。\n由厉害制作\n\n用 fgit clone [github仓库链接，只能是http或https的] 加速仓库克隆。");
+		process.exit();
+}
+
+if (argv[2] == "clone") {
+		argv[2] == "clone" && cloneRe.test(argv[3])?exec("git clone " + argv[3].replace("github.com", "hub.fastgit.org"), function (err, stdout, srderr) {
+				console.log("看来加速还是不错滴2333");
+		}):(function () {
+				console.log("第三个参数有问题哦");
+				process.exit();})()
+} else {
+		git(argv);
+}
